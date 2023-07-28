1f7a00d6cc2a575a4ddd3d4db9b317e68e51a394
nurealammiaji <nurealam.miaji@gmail.com>
2023-07-28 22:58:43 +0600

    line chart added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Dashboard from './components/Dashboard/Dashboard';
 import Navbar from './components/Navbar/Navbar';
 import ProductList from './components/ProductList/ProductList';
 
@@ -8,6 +9,12 @@ function App() {
     <>
       <Navbar></Navbar>
       <ProductList></ProductList>
+      <br /><br /><br />
+      <div>
+        <h1 className='text-3xl font-bold text-center'>Line Chart</h1>
+        <br /><br />
+        <Dashboard></Dashboard>
+      </div>
     </>
   )
 }
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,7 +1,5 @@
 const ProductCard = ({product}) => {
 
-    console.log(product);
-
     return (
         <div className="p-4 text-center bg-purple-200 border rounded-md">
             <div className="mx-auto w-28">
@@ -9,12 +7,12 @@ const ProductCard = ({product}) => {
             </div>
             <h1 className="text-xl">{product?.title}</h1>
             <br />
-            <p>Price: ${product.price}</p>
+            <p>Price: ${product?.price}</p>
             <br />
             <small>Rating: {product?.rating?.rate}</small>
             <br />
             <br />
-            <button className="px-4 py-2 bg-purple-400 rounded-sm">Add to Cart</button>
+            <button className="w-full px-4 py-2 bg-purple-400 rounded-md">Add to Cart</button>
         </div>
     );
 };
